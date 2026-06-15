32c7f2230a5a1c8c5a70fdf3388c01f021f79a96
MarceloYujiRuiz <yujimarcelo62@gmail.com>
2026-06-15 16:18:31 -0300

    feat: Adiciona MonitoramentoController e algumas alterações em MonitoramentoService

---
+++ src/main/java/com/WagaClaude/wagaclaude/controller/MonitoramentoController.java
@@ -0,0 +1,35 @@
+package com.WagaClaude.wagaclaude.controller;
+
+import com.WagaClaude.wagaclaude.model.Monitoramento;
+import com.WagaClaude.wagaclaude.service.MonitoramentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/monitoramento")
+public class MonitoramentoController {
+
+    @Autowired
+    private MonitoramentoService monitoramentoService;
+
+    @GetMapping("/{recursoId}")
+    public ResponseEntity<List<Monitoramento>> listarPorRecurso(@PathVariable Integer recursoId) {
+        List<Monitoramento> metricas = monitoramentoService.listarPorRecurso(recursoId);
+        return ResponseEntity.ok(metricas);
+    }
+
+    @PostMapping("/gerar")
+    public ResponseEntity<String> gerarMetricas(@RequestParam Integer recursoId) {
+        monitoramentoService.gerarMetricasFakePorId(recursoId);
+        return ResponseEntity.ok("Métricas geradas para o recurso " + recursoId);
+    }
+
+    @GetMapping("/criticos")
+    public ResponseEntity<List<Monitoramento>> listarCriticos() {
+        List<Monitoramento> criticos = monitoramentoService.listarCriticos();
+        return ResponseEntity.ok(criticos);
+    }
+}
